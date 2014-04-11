e36013b9c5a91f2926659807b281bea322e2f589
Jim Flanagan <flanagan.james@gmail.com>
2014-04-11 09:27:27 -0600

    row height set

--- CPLTableViewController.m
+++ CPLTableViewController.m
@@ -727,6 +727,7 @@ if (self.suspendSpeechCommands == NO)
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
+    [self.tableView setRowHeight:35.0f];
     return [self.checkListItems count];
 }
 
