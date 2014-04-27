1fb787d57ec9fcb2ba52c4b69835b21bd50d25ee
Jim Flanagan <flanagan.james@gmail.com>
2014-04-27 12:58:09 -0600

    added slideshowview

---
+++ CoPilot/CPLSlideShowViewController.h
@@ -0,0 +1,13 @@
+//
+//  CPLSlideShowViewController.h
+//  CheckOutLoud
+//
+//  Created by James Flanagan on 4/27/14.
+//  Copyright (c) 2014 James Flanagan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPLSlideShowViewController : UIViewController
+
+@end
